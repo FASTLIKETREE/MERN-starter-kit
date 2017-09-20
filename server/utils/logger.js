20c4b4bbb348096f98b180b41c0622ee55f2ec4c
FASTLIKETREE <wesleyaaronhunt@gmail.com>
2017-09-20 14:50:31 -0500

    Server build config

---
+++ server/utils/logger.js
@@ -0,0 +1,49 @@
+/*
+    Let's make the console easier to read
+    text colors
+        black
+        red
+        green
+        yellow
+        blue
+        magenta
+        cyan
+        white
+        gray
+        grey
+    background colors
+        bgBlack
+        bgRed
+        bgGreen
+        bgYellow
+        bgBlue
+        bgMagenta
+        bgCyan
+        bgWhite
+    styles
+        reset
+        bold
+        dim
+        italic
+        underline
+        inverse
+        hidden
+        strikethrough
+*/
+
+const colors = require('colors');
+
+function print(fn, text){
+    console.log(fn(text));
+}
+
+console.log(typeof colors.green);
+const log = (text) => print(colors.green, text);
+const debug = (text) => print(colors.blue, text);
+const warn = (text) => print(colors.bgYellow.black, text);
+const error = (text) => print(colors.bgBlack.underline.red, text);
+
+module.exports.log = log;
+module.exports.debug = debug;
+module.exports.warn = warn;
+module.exports.error = error;
